9da79672b048648467645239a6459b2aeca2ed0b
Juan Cortes <juancoob@gmail.com>
2017-05-01 19:01:15 -0400

    second test for voting component pass!!

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -10,7 +10,8 @@ class Voting extends Component {
 		return (
 			<div className="voting">
 				{this.getPair().map(entry =>
-					<button key={entry}>
+					<button key={entry}
+									onClick={() => this.props.vote(entry)}>
 						<h1>{entry}</h1>
 					</button>
 				)}
--- voting-client/tests/components/Voting_spec.test.js
+++ voting-client/tests/components/Voting_spec.test.js
@@ -12,3 +12,14 @@ it('renders Voting component as two buttons with no errors', () => {
   expect(mount(votingComponent).find('button').at(0).text()).toBe('Trainspotting');
   expect(mount(votingComponent).find('button').at(1).text()).toBe('28 Days Later');
 })
+
+it('invokes a callback when a button is clicked', () => {
+	var votedWith;
+	const vote = (entry) => votedWith = entry;
+
+	const votingComponent = mount(<Voting pair={['Trainspotting', '28 Days Later']} vote={vote} />);
+	votingComponent.find('button').at(0).simulate('click');
+
+	expect(votedWith).toBe('Trainspotting');
+
+})
